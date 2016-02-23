bf0e02783503d375e949150dee1cbaae316435e7
Erneker Josef <Josef.Erneker@ysoft.com>
2016-02-23 10:35:11 +0100

    Add opening anybar port on habit sync

--- sync_habits.rb
+++ sync_habits.rb
@@ -6,6 +6,21 @@ include AsanaHelper
 
 @params = {:action => 'Sync Habits'}
 communicate do
-  sync_habits if @config[:asana][:habits_active]
+  begin
+    if @config[:asana][:anybar_active]
+      port = pop_port
+      if port
+        start_session(port, 'Habits synchronisation is running')
+        sleep 2
+        anybar('cyan', port)
+      end
+    end
+    sync_habits if @config[:asana][:habits_active]
+  ensure
+    if @config[:asana][:anybar_active] && port
+      anybar('quit', port)
+      return_port(port)
+    end
+  end
 end
 puts 'Habits synced'
